34b0dcb44e4dafdad39050e5c18d2120bdb21966
shishir <shisirsharma22@gmail.com>
2024-08-13 23:13:53 +0545

    Add customer, order, and metrics routes

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const multipart = require('connect-multiparty')
+// const multipart = require('connect-multiparty')
 const connectDb = require('./database/connectDb')
 const cors = require('cors')
 const path = require('path')
-
-
 
 const app = express()
 dotenv.config()
 
 app.use(express.json())
-app.use(multipart())
+// app.use(multipart())
 
 
 app.use(express.static("./public"))
@@ -34,6 +32,9 @@ app.get('/', (req, res) => res.send({ "message": 'Hello World!' }))
 // Routers
 app.use('/api/user', require('./routes/userRoutes'))
 app.use('/api/product', require('./routes/productRoutes'))
+app.use('/api/customer', require('./routes/customerRoutes'))
+app.use('/api/order', require('./routes/orderRoutes'))
+app.use('/api/metrics', require('./routes/metricsRoutes'))
 
 
 app.listen(port, () => console.log(`Sajilo Sales listening on port ${port}!`))
